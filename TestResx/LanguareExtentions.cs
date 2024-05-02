095ddfc0b30b220f2a52d8476cc87b04e29daf1e
Alexandr <SoftStar2004@gmail.com>
2024-05-02 10:36:23 +0300

    Add project files.

---
+++ TestResx/LanguareExtentions.cs
@@ -0,0 +1,168 @@
+﻿using DeepL;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources.NetStandard;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace TranslateResx
+{
+    public class ResourceKey
+    {
+        public string Key { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class LanguageReaderExtensions : IDisposable
+    {
+        private readonly ResXResourceReader _reader;
+        private readonly string path;
+        private bool disposedValue;
+
+        public LanguageReaderExtensions(string path)
+        {
+            this.path = path;
+            _reader = new ResXResourceReader(path);
+        }
+
+        public static IEnumerable<string> FindResxFiles(string path, string lang)
+        {
+            return Directory.EnumerateFiles(path, $"*.{lang}.resx", SearchOption.AllDirectories);
+        }
+
+
+        public IEnumerable<ResourceKey> GetResourceKeys()
+        {
+            var keys = new List<ResourceKey>();
+            foreach (DictionaryEntry entry in _reader)
+            {
+                if (entry.Value != null)
+                {
+                    keys.Add(new ResourceKey { Key = entry.Key.ToString() ?? string.Empty, Type = entry.Value.GetType().ToString() });
+                }
+            }
+            return keys;
+        }
+
+        public string? GetResourceValue(string key)
+        {
+            foreach (DictionaryEntry entry in _reader)
+            {
+                if (entry.Key.ToString() == key)
+                {
+                    return entry.Value.ToString();
+                }
+            }
+            return null;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _reader.Close();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~LanguageReaderExtensions()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    public class LanguageWriterExtensions : IDisposable
+    {
+        private readonly ResXResourceWriter _writer;
+        private readonly string path;
+        private bool disposedValue;
+
+        public LanguageWriterExtensions(string path)
+        {
+            this.path = path;
+            _writer = new ResXResourceWriter(path);
+        }
+
+        public static string GetResxFileName(string fileName, string langCode)
+        {
+            var regex = new Regex(@"\.\w{2}-\w{2}\.resx");
+            return regex.Replace(fileName, $".{langCode}.resx");
+        }
+
+        public void SetResourceValue(string key, string newValue)
+        {
+            _writer.AddResource(key, newValue);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _writer.Close();
+                    _writer.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~LanguageWriterExtensions()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    public class TranslatorExtensions
+    {
+        private readonly Translator _translator;
+
+        public TranslatorExtensions(string authKey)
+        {
+            _translator = new Translator(authKey);
+        }
+
+        public string Translate(string textTranslate, string langFrom, string langTo )
+        {
+            var from = LanguageCode.RemoveRegionalVariant(langFrom);
+            var to = LanguageCode.RemoveRegionalVariant(langTo);
+            var translatedText = _translator.TranslateTextAsync(
+                  textTranslate,
+                  from,
+                  to).Result;
+            return translatedText.Text;
+        }
+    }
+}
---
+++ TestResx/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+using System.Globalization;
+using System.Resources;
+using System.Resources.NetStandard;
+using System.Xml.Linq;
+using TranslateResx;
+
+Console.WriteLine("Start translator resource");
+
+if (args.Length < 3)
+{
+    Console.WriteLine("Usage: Translator <Folder path> <Code lang from ('en-US','uk-UA')> <Code lang to ('en-US','uk-UA')>");
+    return;
+}
+
+var variableValue = Environment.GetEnvironmentVariable("DEPL_KEY_API");
+if (string.IsNullOrEmpty(variableValue))
+{
+    Console.WriteLine("Environment variable DEPL_KEY_API not found, create Environment variable 'DEPL_KEY_API'");
+    return;
+}
+
+string resxFolderPath = args[0];
+string langFrom = args[1];
+string langTo = args[2];
+
+var resxFiles = LanguageReaderExtensions.FindResxFiles(resxFolderPath, langFrom);
+if (resxFiles == null)
+{
+    Console.WriteLine("No files for translate found");
+    return;
+}
+
+var translator = new TranslatorExtensions(variableValue);
+
+foreach (var resxFile in resxFiles)
+{
+    Console.WriteLine($"Translate file: {resxFile}");
+    var newNames = LanguageWriterExtensions.GetResxFileName(resxFile, langTo);
+
+    using (var resxReader = new LanguageReaderExtensions(resxFile))
+    using (var resxWriter = new LanguageWriterExtensions(newNames))
+    {
+        var keys = resxReader.GetResourceKeys();
+        foreach (var key in keys)
+        {
+            Console.WriteLine($"    Translate key: {key.Key} {key.Type}");
+            var value = resxReader.GetResourceValue(key.Key);
+            if (value != null)
+            {
+                var translatedValue = translator.Translate(value, langFrom, langTo);
+                if (translatedValue != null)
+                {
+                    resxWriter.SetResourceValue(key.Key, translatedValue);
+                }
+            }
+            //Console.SetCursorPosition(0, Console.CursorTop - 1);
+        }
+    }
+    Console.WriteLine($"Finish translate file: {newNames}");
+}
